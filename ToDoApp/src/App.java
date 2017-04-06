0148639e4db858cf9cc586943260352189d38833
Nagy Dora <dorinagy06@gmail.com>
2017-04-06 15:07:03 +0200

    add print usage function

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -6,12 +6,26 @@ public class App {
   public static void main(String[] args) {
     ToDoList myToDo = new ToDoList();
     ArgumentHandler handler = new ArgumentHandler(args);
-    handler.noArgument("");
+    if(args.length == 0) {
+      printUsage();
+    }
     if(handler.contains("l")) {
       myToDo.getFile();
-    }
-    if(handler.contains("a") && args.length < 3) {
+    } else if(handler.contains("a") && args.length < 3) {
       System.out.println("Unable to add: no task provided");
+    } else if(handler.contains("a") && args.length > 3){
+      myToDo.addNewTask(args.toString().substring(3));
     }
+  }
+
+  public static void printUsage() {
+    System.out.println("Java Todo application");
+    System.out.println("=====================");
+    System.out.println();
+    System.out.println("Command line arguments:");
+    System.out.println(" -l   Lists all the tasks");
+    System.out.println(" -a   Adds a new task");
+    System.out.println(" -r   Removes an task");
+    System.out.println(" -c   Completes an task");
   }
 }
