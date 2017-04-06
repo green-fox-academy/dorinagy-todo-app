9a115ee1b2085cdaa0deb4ea8e7602c459052e9b
Nagy Dora <dorinagy06@gmail.com>
2017-04-06 12:46:51 +0200

    move print usage information

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,18 +1,15 @@
-import java.util.Arrays;
 
 /**
  * Created by Nagy Dóra on 2017.04.06..
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println();
-    System.out.println("Java Todo application");
-    System.out.println("=====================");
-    System.out.println();
-    System.out.println("Command line arguments:");
-    System.out.println(" -l  Lists all the tasks");
-    System.out.println(" -a  Adds a new task");
-    System.out.println(" -r  Removes a task");
-    System.out.println(" -c  Completes a task");
+    ToDoList myToDo = new ToDoList();
+    ArgumentHandler handler = new ArgumentHandler(args);
+    if(handler.contains("l")) {
+      myToDo.getFile();
+    }
+    handler.noArgument("");
+
   }
 }
--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,5 +1,31 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Nagy Dóra on 2017.04.06..
  */
 public class ToDoList {
+  public void getFile() {
+    try {
+      Path filePath = Paths.get("../files/data.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      List<String> myList = new ArrayList<>();
+
+      for (String line : lines) {
+        myList.add(line);
+      }
+
+      int number = 1;
+      for(String line : myList) {
+        System.out.println(number + " - " + line);
+        number++;
+      }
+
+    } catch(Exception e) {
+      System.out.println("Error" + e.getClass());
+    }
+  }
 }
