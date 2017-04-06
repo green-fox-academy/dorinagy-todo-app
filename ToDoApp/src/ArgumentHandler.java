0d550fe23a0ffeae1c44990d9cf041a89de3f9c7
Nagy Dora <dorinagy06@gmail.com>
2017-04-06 12:48:24 +0200

    moved usage information

--- ToDoApp/src/ArgumentHandler.java
+++ ToDoApp/src/ArgumentHandler.java
@@ -7,6 +7,23 @@ public class ArgumentHandler {
   public ArgumentHandler(String[] args) {
     this.args = args;
   }
+
+  public boolean noArgument(String argument) {
+    if(args.length == 0) {
+      System.out.println();
+      System.out.println("Java Todo application");
+      System.out.println("=====================");
+      System.out.println();
+      System.out.println("Command line arguments:");
+      System.out.println(" -l  Lists all the tasks");
+      System.out.println(" -a  Adds a new task");
+      System.out.println(" -r  Removes a task");
+      System.out.println(" -c  Completes a task");
+      return true;
+    }
+    return false;
+  }
+
   public boolean contains(String argument) {
     if (args.length > 0) {
       for (String arg : args) {
