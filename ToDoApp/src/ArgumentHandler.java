717b53db6cbc5d274cf8e3a4cadbf00958448c2a
Nagy Dora <dorinagy06@gmail.com>
2017-04-06 11:54:55 +0200

    add argument handler exercise

---
+++ ToDoApp/src/ArgumentHandler.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Nagy Dóra on 2017.04.06..
+ */
+public class ArgumentHandler {
+  String[] args;
+
+  public ArgumentHandler(String[] args) {
+    this.args = args;
+  }
+  public boolean contains(String argument) {
+    if (args.length > 0) {
+      for (String arg : args) {
+        if (arg.startsWith("-") && arg.contains(argument))
+          return true;
+      }
+    }
+    return false;
+  }
+}
